e48265fd53689226465ed3dae669da8eccf87ac9
miguelmoraisjr <miguelj@ciandt.com>
2022-07-19 16:31:28 -0300

    Ajustes

--- 0x02/password/src/PersonTest.java
+++ 0x02/password/src/PersonTest.java
@@ -4,13 +4,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 import org.junit.jupiter.params.provider.ValueSource;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class PersonTest {
 
-    private String nome;
-    private String senha;
+
     private static Person person;
 
 
@@ -21,47 +21,47 @@ public class PersonTest {
 
     @ParameterizedTest
     @ValueSource(strings = {"PaulMcCartney2", "NeilArms2"})
-    public void check_user_valid(ArgumentsAccessor argumentsAccessor) {
-        person.setNome(argumentsAccessor.getString(0));
+    public void check_user_valid(String nome) {
+        person.setNome(nome);
         boolean check = person.checkUser();
         Assertions.assertTrue(check);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"Paul#McCartney", "Neil@Arms"})
-    public void check_user_not_valid(ArgumentsAccessor argumentsAccessor) {
-        person.setNome(argumentsAccessor.getString(0));
+    public void check_user_not_valid(String nome) {
+        person.setNome(nome);
         boolean check = person.checkUser();
         Assertions.assertFalse(check);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"123456789", "#$%1234"})
-    public void does_not_have_letters(ArgumentsAccessor argumentsAccessor) {
-        person.setSenha(argumentsAccessor.getString(0));
+    public void does_not_have_letters(String senha) {
+        person.setSenha(senha);
         boolean check = person.checkPassword();
         Assertions.assertFalse(check);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"Abcabcdefgh@", "#hbtn@%tc"})
-    public void does_not_have_numbers(ArgumentsAccessor argumentsAccessor) {
-        person.setSenha(argumentsAccessor.getString(0));
+    public void does_not_have_numbers(String senha) {
+        person.setSenha(senha);
         boolean check = person.checkPassword();
         Assertions.assertFalse(check);
     }
     @ParameterizedTest
     @ValueSource(strings = {"Abc@123", "12$@hbt"})
-    public void does_not_have_eight_chars(ArgumentsAccessor argumentsAccessor) {
-        person.setSenha(argumentsAccessor.getString(0));
+    public void does_not_have_eight_chars(String senha) {
+        person.setSenha(senha);
         boolean check = person.checkPassword();
         Assertions.assertFalse(check);
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"abC123456$", "Hbtn@1234", "Betty@1#2", "Hbtn@123"})
-    public void check_password_valid(ArgumentsAccessor argumentsAccessor) {
-        person.setSenha(argumentsAccessor.getString(0));
+    public void check_password_valid(String senha) {
+        person.setSenha(senha);
         boolean check = person.checkPassword();
         Assertions.assertTrue(check);
     }
